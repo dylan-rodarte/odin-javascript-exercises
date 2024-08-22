66063708ce4cb695307e00851ad5e9941229cdb7
dylan-rodarte <173209237+dylan-rodarte@users.noreply.github.com>
2024-08-22 18:30:28 -0500

    feat(removeFromArray): Create basic function to remove inputs from an array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,20 @@
 const removeFromArray = function() {
+    let initialArray = arguments[0];
+    let toBeRemoved = Array.from(arguments);
+    toBeRemoved.shift(); //removes contents of initialArray from arguments
+    let finalArray = [];
+    
+    outer: for (let i = 0; i < initialArray.length; i++){
+         for (let j = 0; j < toBeRemoved.length; j++){
+            if (initialArray[i] === toBeRemoved[j]){
+                continue outer;
+            }
+         }
+         finalArray.push(initialArray[i]);
+    }
+    return finalArray
 };
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
